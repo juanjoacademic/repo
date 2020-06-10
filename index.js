3bf1c6bb99e6a9f988276c7fb315748cf8ddb487
Juanjo <juanjo.academic@gmail.com>
2020-06-10 15:28:38 -0500

    Actualización hora Amsterdam

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var time = require('time');
 
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date();
-    currentTime.setTimezone("America/New_York");
+    currentTime.setTimezone("Europe/Amsterdam");
     callback(null, {
         statusCode: '200',
-        body: 'La hora exacta en New York es: ' + currentTime.toString(),
+        body: 'The Amsterdam time is: ' + currentTime.toString(),
     });
 };
