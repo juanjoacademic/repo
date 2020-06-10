fbba544f951d257b65b21759a2518c7f4763754a
Juanjo <juanjo.academic@gmail.com>
2020-06-10 12:21:42 -0500

    Actualización archivos

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var time = require('time');
 
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date();
-    currentTime.setTimezone("XXX/XXX");
+    currentTime.setTimezone("America/New_York");
     callback(null, {
         statusCode: '200',
-        body: 'La hora exacta en XXXX es: ' + currentTime.toString(),
+        body: 'La hora exacta en New York es: ' + currentTime.toString(),
     });
 };
