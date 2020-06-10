b16df433d5f30accb66ef086062f2a9009606a94
Johan Andrey Baron <jabaral@amazon.com>
2020-06-10 01:15:52 -0500

    fix

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var time = require('time');
 
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date();
-    currentTime.setTimezone("XXXX/XXXX");
+    currentTime.setTimezone("America/Bogota");
     callback(null, {
         statusCode: '200',
-        body: 'La hora exacta en XXXX es: ' + currentTime.toString(),
+        body: 'La hora exacta en Bogotá es: ' + currentTime.toString(),
     });
 };
